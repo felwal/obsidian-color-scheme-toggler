f8637d583069a41b2614ee430b75e784a03d8f12
Felix Wallin <nils.felix@gmail.com>
2022-10-16 13:51:44 +0200

    Add toggle-color-scheme command

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { Plugin } from "obsidian";
 
 export default class ColorSchemeTogglerPlugin extends Plugin {
+  COLOR_SCHEME_DARK = "obsidian";
+  COLOR_SCHEME_LIGHT = "moonstone";
+
   async onload() {
     console.log("loading plugin");
 
@@ -12,5 +15,24 @@ export default class ColorSchemeTogglerPlugin extends Plugin {
   }
 
   loadCommands() {
+    this.addCommand({
+      id: "toggle-color-scheme",
+      name: "Toggle color scheme",
+      callback: () => {
+        this.toggleColorScheme();
+      }
+    });
+  }
+
+  toggleColorScheme() {
+    this.app.changeTheme(this.getInvertedColorScheme());
+  }
+
+  getInvertedColorScheme() {
+    return this.getColorScheme() === this.COLOR_SCHEME_LIGHT ? this.COLOR_SCHEME_DARK : this.COLOR_SCHEME_LIGHT;
+  }
+
+  getColorScheme() {
+    return this.app.vault.getConfig("theme");
   }
 }
